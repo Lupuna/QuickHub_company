64c919c2b5952334e7a9609481762607b2005da2
Lupuna <terragod427@gmail.com>
2024-11-01 20:32:06 +0700

    completion of preparation for deployment

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,12 +1,15 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('company/registration/', include('jwt_registration.urls')),
-    path('company/', include('company.urls'))
+    path('company/', include('company.urls')),
+    path('schema/', SpectacularAPIView.as_view(), name='api_schema'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='api_schema'), name='swagger-ui'),
 ]
 
 if settings.DEBUG:
--- core/jwt_registration/views.py
+++ core/jwt_registration/views.py
@@ -1,10 +1,14 @@
 from rest_framework.mixins import CreateModelMixin, UpdateModelMixin, RetrieveModelMixin
 from rest_framework.viewsets import GenericViewSet
 from jwt_registration.models import User
+from drf_spectacular.utils import extend_schema
 
 from jwt_registration.serializers import UserSerializer
 
 
+@extend_schema(
+    tags=["Create users"]
+    )
 class RegistrationAPIViewSet(GenericViewSet, CreateModelMixin, UpdateModelMixin, RetrieveModelMixin):
     queryset = User.objects.all()
     serializer_class = UserSerializer
