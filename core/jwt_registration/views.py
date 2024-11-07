839fc117c8f4721822961fa30b9647f6d1adeca8
Lupuna <terragod427@gmail.com>
2024-11-07 19:57:25 +0700

    create two commits pattern for user creation without test coverage

--- core/jwt_registration/views.py
+++ core/jwt_registration/views.py
@@ -1,3 +1,7 @@
+from django.db import transaction
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from jwt_registration.models import User
 from drf_spectacular.utils import extend_schema
@@ -11,3 +15,40 @@ from jwt_registration.serializers import UserSerializer
 class RegistrationAPIViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @action(detail=False, methods=['post'], url_path='create')
+    def create_user(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        return Response({'status': 'created'}, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['post'], url_path='confirm')
+    def confirm_user(self, request, *args, **kwargs):
+        user_email = request.data.get('email')
+
+        if not user_email:
+            return Response({'error': 'User creation not initiated'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            with transaction.atomic():
+                user = User.objects.get(email=user_email)
+                user.is_active = True
+                user.save()
+            return Response({'status': 'confirmed'}, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    @action(detail=False, methods=['post'], url_path='rollback')
+    def rollback_user(self, request, *args, **kwargs):
+        user_id = request.session.get('user_id')
+
+        if not user_id:
+            return Response({'error': 'User creation not initiated'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user = User.objects.get(id=user_id).delete()
+            return Response({'status': 'rolled back'}, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
