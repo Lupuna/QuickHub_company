3e57e88a0f2589eafa11e11836dd610c60094fcd
Ali <pridanov.ali.job@gmail.com>
2024-11-23 00:32:07 +0700

    updated get_users_email_only(added pos and deps) with no tests

--- core/users/serializers.py
+++ core/users/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
 from jwt_registration.models import User
 from company.serializers import CompanyForUserSerializer
+from company.serializers import PositionSerializer, DepartmentSerializer
 
 
 class OnlyUserEmailSerializer(serializers.ModelSerializer):
+    positions = PositionSerializer(many=True)
+    departments = DepartmentSerializer(many=True)
 
     class Meta:
         model = User
-        fields = ('email', )
+        fields = ('email', 'positions', 'departments')
 
 
 class UserCompanySerializer(serializers.ModelSerializer):
