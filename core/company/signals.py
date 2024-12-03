60e255fb48e1d7f9a4741f16962a9d99b2ca7137
Ali <pridanov.ali.job@gmail.com>
2024-12-03 22:09:35 +0700

    save users info by dep endpoint

--- core/company/signals.py
+++ core/company/signals.py
@@ -11,11 +11,13 @@ def create_company_position(instance, action, **kwargs):
     user_creator_id = list(kwargs.get('pk_set', []))[0]
     if action == 'pre_add':
         creator = User.objects.get(id=user_creator_id)
-        creators_company_with_same_title = Company.objects.filter(title=instance.title, users=creator).exclude(id=instance.id)
-        creators_positions_list = Position.objects.filter(company__in=creators_company_with_same_title).values_list('access_weight')
+        creators_company_with_same_title = Company.objects.filter(
+            title=instance.title, users=creator).exclude(id=instance.id)
+        creators_positions_list = Position.objects.filter(
+            company__in=creators_company_with_same_title).values_list('access_weight')
         if (0,) in creators_positions_list:
             instance.delete()
-            raise ValidationError({'error':'data_update is required'})
+            raise ValidationError({'error': 'data_update is required'})
     if action == 'post_add':
         user_creator_id = list(kwargs.get('pk_set', []))[0]
         creator = User.objects.get(id=user_creator_id)
--- core/company/views.py
+++ core/company/views.py
@@ -7,16 +7,18 @@ from rest_framework.generics import GenericAPIView
 from rest_framework import status
 from company.serializers import (
     CompanySerializer, PositionSerializer, DepartmentSerializer,
-    ProjectSerializer, ProjectPostSerializer)
+    ProjectSerializer, ProjectPostSerializer, ProfileUserForDepSerializer)
 from users.serializers import UserEmailSerializer
 from company.models import Company, Position, Project, Department
 from jwt_registration.models import User
 from users.serializers import OnlyUserEmailSerializer
+from django.conf import settings
+import requests
 
 
 @extend_schema(
     tags=["Company"]
-    )
+)
 class CompanyAPIViewSet(ModelViewSet):
     serializer_class = CompanySerializer
     queryset = Company.objects.prefetch_related('users').all()
@@ -35,7 +37,7 @@ class CompanyAPIViewSet(ModelViewSet):
 
 @extend_schema(
     tags=["Position"]
-    )
+)
 class PositionAPIViewSet(ModelViewSet):
     serializer_class = PositionSerializer
 
@@ -45,7 +47,7 @@ class PositionAPIViewSet(ModelViewSet):
 
 @extend_schema(
     tags=["Project"]
-    )
+)
 class ProjectAPIViewSet(ModelViewSet):
     http_method_names = ['get', 'post', 'patch', 'delete']
 
@@ -58,7 +60,8 @@ class ProjectAPIViewSet(ModelViewSet):
         company_id = self.kwargs.get('company_pk')
         prefetch_positions = Prefetch(
             'positions',
-            queryset=Position.objects.filter(company=company_id).prefetch_related('project_positions')
+            queryset=Position.objects.filter(
+                company=company_id).prefetch_related('project_positions')
             .only('id', 'title', 'access_weight', 'project_positions__project_access_weight')
         )
         return Project.objects.prefetch_related(prefetch_positions, 'users').filter(company=company_id)
@@ -66,12 +69,34 @@ class ProjectAPIViewSet(ModelViewSet):
 
 @extend_schema(
     tags=["Department"]
-    )
+)
 class DepartmentAPIViewSet(ModelViewSet):
     serializer_class = DepartmentSerializer
 
     def get_queryset(self):
         return Department.objects.prefetch_related('users').filter(company=self.kwargs['company_pk'])
+
+    @action(methods=['get'], detail=False, url_path='(?P<dep_pk>[^/.]+)/users-info-by-dep', url_name='get_users_info_by_dep')
+    def get_users_info_by_dep(self, request, dep_pk, company_pk):
+        department = self.get_queryset().get(id=dep_pk)
+        users_emails = [user.get('email', None)
+                        for user in department.users.values('email')]
+
+        url = settings.REGISTRATION_SERVICE_URL.format(
+            f'profile/api/v1/profile/users-info-by-company/{company_pk}')
+        response = requests.get(url=url)
+        if response.status_code != 200:
+            return Response({'detail': "company info wasn't get"}, status=response.status_code)
+        response_data = response.json()
+
+        users_info_by_dep = [
+            user_info for user_info in response_data if user_info['email'] in users_emails]
+        for user_info in users_info_by_dep:
+            del user_info['departments']
+        users_info_by_dep = ProfileUserForDepSerializer(
+            users_info_by_dep, many=True)
+
+        return Response(users_info_by_dep.data, status=status.HTTP_200_OK)
 
 
 @extend_schema(
@@ -79,11 +104,13 @@ class DepartmentAPIViewSet(ModelViewSet):
 )
 class UserInCompanyValidateView(GenericAPIView):
     serializer_class = UserEmailSerializer
+
     def post(self, request, *args, **kwargs):
         serializer = UserEmailSerializer(data=request.data)
         if serializer.is_valid():
-            user_in_company = Company.objects.get(id=self.kwargs['company_pk']).users.filter(email=serializer.data.get('email', None)).exists()
+            user_in_company = Company.objects.get(id=self.kwargs['company_pk']).users.filter(
+                email=serializer.data.get('email', None)).exists()
             if user_in_company:
-                return Response({'status':'User in company'},status=status.HTTP_200_OK)
+                return Response({'status': 'User in company'}, status=status.HTTP_200_OK)
             else:
-                return Response({'status':'User is not in company'},status=status.HTTP_400_BAD_REQUEST)
+                return Response({'status': 'User is not in company'}, status=status.HTTP_400_BAD_REQUEST)
