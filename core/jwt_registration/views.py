3d7824d7203a4b79a3d535b66e08f210319b9d27
TheAppleKingy <pridanov.ali.job@gmail.com>
2024-12-19 13:43:35 +0700

    prepare to test email update feature

--- core/jwt_registration/views.py
+++ core/jwt_registration/views.py
@@ -52,41 +52,62 @@ class RegistrationAPIViewSet(
 
         return Response({'status': 'created'}, status=status.HTTP_200_OK)
 
+    @action(methods=['post'], detail=False, url_path='update')
+    def update_user(self, request, *args, **kwargs):
+        email = request.data.get('email', None)
+        new_email = request.data.get('new_email', None)
+        if not (email and new_email):
+            return Response({"error": f"Current email: {email}\nNew email: {new_email}"}, status=status.HTTP_400_BAD_REQUEST)
+
+        self.handle_cache(email, 'set', request.data)
+
+        return Response({'status': 'email updated'}, status=status.HTTP_200_OK)
+
     @action(detail=False, methods=['post'], url_path='confirm')
     def confirm_user(self, request, *args, **kwargs):
         email = get_email_or_400(request)
-
         user_data = self.handle_cache(email, 'get')
+        if not (email == user_data['email']):
+            return Response({'error': 'Cached email not equal got'}, status=status.HTTP_400_BAD_REQUEST)
         if user_data:
             self.handle_cache(email, 'delete')
 
             user = get_user_model().objects.filter(email=email)
             if user.exists():
                 user[0].is_registered = True
+                user[0].email = user_data['new_email']
                 user[0].save()
-                return Response({'status': 'already_confirmed'}, status=status.HTTP_200_OK)
+                return Response({'status': 'Email update confirmed'}, status=status.HTTP_200_OK)
+    
             self.perform_create(self.get_serializer(data=user_data))
-            return Response({'status': 'confirmed'}, status=status.HTTP_200_OK)
+            return Response({'status': 'Creation confirmed'}, status=status.HTTP_200_OK)
         return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
 
     @action(detail=False, methods=['post'], url_path='rollback')
     def rollback_user(self, request, *args, **kwargs):
         email = get_email_or_400(request)
-
+        new_email = request.data.get('new_email', None)
+        move = request.data.get('move', None)
         self.handle_cache(email, 'delete')
-        user = User.objects.filter(email=email)
-        if user.exists():
-            user.delete()
-        return Response({'status': 'rolled back'}, status=status.HTTP_200_OK)
 
-    @action(methods=['post'], detail=False, url_path='update')
-    def update_important_data(self, request, *args, **kwargs):
-        user_email = request.data.get('email', None)
-        if not user_email:
-            return Response({"error": "Email or password wasn't get"}, status=status.HTTP_400_BAD_REQUEST)
+        if move != 'confirm':
+            return Response({'data': 'First commit rolled back'}, status=status.HTTP_200_OK)
 
-        user = User.objects.get(email=user_email)
-        user.email = user_email
-        user.save()
+        if new_email:
+            user_after_update = User.objects.filter(email=new_email)
+            if user_after_update:
+                user_after_update[0].email = email
+                user_after_update[0].save()
+                return Response({'data': 'Update confirm rolled back(made email old)'}, status=status.HTTP_200_OK)
+            return Response({"data": "Update confirm rolled back"}, status=status.HTTP_200_OK)
+        
+        created_user = User.objects.filter(email=email)
+        if created_user:
+            created_user[0].delete()
+            return Response({'data':"Create rolled back(user deleted)"}, status=status.HTTP_200_OK)
+        return Response({"data":"Create rolled back"}, status=status.HTTP_200_OK)
 
-        return Response({'status': 'email updated'}, status=status.HTTP_200_OK)
+
+
+
+
